2d904f182b170da9b8c2e9a1bf855f1d1f55ee4b
Erick Hernandez <takashi.onimaru@gmail.com>
2020-09-19 16:28:49 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
-const router = express.Router();
-
-router.post('/', async (req, res) => {
-    console.log("Se conecto");
-})
 
 var app = express();
-
-app.use(router);
 app.use("/", express.static('public'));
 
 
