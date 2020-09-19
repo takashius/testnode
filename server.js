fc0fe479245a56a16651aca69cfc41c55225f9e5
Erick Hernandez <takashi.onimaru@gmail.com>
2020-09-19 16:23:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,3 @@
-/*const express = require('express');
-const bodyParser = require('body-parser');
-const db = require('./db');
-const config = require('./config');
-
-const router = require('./network/routes');
-
-db(config.dbUrl);
-
-var app = express();
-app.use(bodyParser.json());
-router(app);
-
-app.use(config.publicRoute, express.static('public'));
-
-app.listen(config.port, function(){
-    console.log('La app esta escuchando en ' + config.host + ':' + config.port);
-});*/
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
